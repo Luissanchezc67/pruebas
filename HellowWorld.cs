d725f7d38e71986145d1f7b8cc3fa3b55ab8ecd5
Luissanchezc67 <luissanchezc67@gmail.com>
2025-08-24 01:05:01 +0200

    basicos

--- HellowWorld.cs
+++ HellowWorld.cs
@@ -12,6 +12,24 @@ namespace CSharpHelloWorld
 
             string myString = "Esto es una cadena de texto";
             Console.WriteLine(myString);
+
+            int myInt = 42;
+            myInt = myInt + 8;
+            Console.WriteLine(myInt);
+            Console.WriteLine(myInt - 1);
+            Console.WriteLine(myInt);
+
+            double myDouble = 6.5;
+            Console.WriteLine(myDouble);
+
+            float myFloat = 6.5f;
+            Console.WriteLine(myFloat);
+
+            Console.WriteLine(myInt + myDouble + myFloat);
+
+            dynamic myDynamic = 6;
+            myDynamic = "Ahora soy una cadena de texto";
+            Console.WriteLine(myDynamic);
         }
     }
 }
